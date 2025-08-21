856ccab9799ceafca5d830622317a4ed60b47ccc
Ashlye02 <asjoorla@outlook.es>
2025-08-20 21:09:10 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width ="250px";
+}
+// Funcion para cerrar el sidenav
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  } 
+
+ 
